43340656f91d844d35c6923c905e78904a9958ff
A. van der Pluijm <info@urbanlink.nl>
2015-01-25 18:14:30 +0100

    Remove views dependency and views folder. Fixes #6
    Signed-off-by: A. van der Pluijm <info@urbanlink.nl>

--- server/config/express.js
+++ server/config/express.js
@@ -19,9 +19,6 @@ var cors = require('cors');
 module.exports = function(app) {
   var env = app.get('env');
 
-  app.set('views', config.root + '/server/views');
-  app.engine('html', require('ejs').renderFile);
-  app.set('view engine', 'html');
   app.use(compression());
   app.use(bodyParser.urlencoded({ extended: false }));
   app.use(bodyParser.json());
