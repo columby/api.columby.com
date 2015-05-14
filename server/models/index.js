8e661b14f456431c92dbbac4184e573e984a8818
A. van der Pluijm <info@urbanlink.nl>
2015-05-14 23:12:52 +0200

    Collection permissions
    Signed-off-by: A. van der Pluijm <info@urbanlink.nl>

--- server/models/index.js
+++ server/models/index.js
@@ -21,20 +21,6 @@ var sequelize = new Sequelize(config.db.uri, {
   }
 );
 
-/**
- *
- * Authenticate to the database
- *
- **/
-sequelize
-  .authenticate()
-  .complete(function(err) {
-    if (!!err) {
-      console.log('Unable to connect to the database:', err)
-    } else {
-      console.log('Postgres; Connection has been established successfully.')
-    }
-  });
 
 /**
  *
